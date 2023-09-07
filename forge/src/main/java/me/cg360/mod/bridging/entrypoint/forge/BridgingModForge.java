c7de20c40a5c320c68a4b0490d5a8ba0f4eaa52f
Will <16578982+CloudG360@users.noreply.github.com>
2023-09-07 01:45:15 +0100

    Keybindings now work for Forge (+ fixed resource pack error)

--- forge/src/main/java/me/cg360/mod/bridging/entrypoint/forge/BridgingModForge.java
+++ forge/src/main/java/me/cg360/mod/bridging/entrypoint/forge/BridgingModForge.java
@@ -1,25 +1,37 @@
 package me.cg360.mod.bridging.entrypoint.forge;
 
+import com.mojang.logging.LogUtils;
+import me.cg360.mod.bridging.BridgingKeyMappings;
 import me.cg360.mod.bridging.BridgingMod;
 import me.cg360.mod.bridging.compat.forge.DynamicCrosshairCompat;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BridgingMod.MOD_ID)
-@Mod.EventBusSubscriber(modid = BridgingMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BridgingModForge {
 
     private static final String DYNAMIC_CROSSHAIR_MOD = "dynamiccrosshair";
 
-    @SubscribeEvent
+    public BridgingModForge() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::registerBindings);
+    }
+
+
     public void init(FMLClientSetupEvent event) {
         BridgingMod.init();
 
         if(ModList.get().isLoaded(DYNAMIC_CROSSHAIR_MOD))
             InterModComms.sendTo(DYNAMIC_CROSSHAIR_MOD, "register_api", DynamicCrosshairCompat::new);
+    }
+
+
+    public void registerBindings(RegisterKeyMappingsEvent event) {
+        BridgingKeyMappings.forEachKeybindingDo(event::register);
     }
 
 }
