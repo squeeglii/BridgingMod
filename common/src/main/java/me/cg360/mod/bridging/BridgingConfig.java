aa2a1a4da4bf381968467bbc7f5d97003e33dd86
Will <16578982+CloudG360@users.noreply.github.com>
2023-12-21 02:15:43 +0000

    Collection of config suggestions: - Allow outlines outside of F3 menu (Discord) - Outline can now be made less faint (Discord) - Disallow bridging over non-solids (Curse) - Force bridging to only work when crouching (Discord)

--- common/src/main/java/me/cg360/mod/bridging/BridgingConfig.java
+++ common/src/main/java/me/cg360/mod/bridging/BridgingConfig.java
@@ -2,16 +2,33 @@ package me.cg360.mod.bridging;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "bridgingmod")
 public class BridgingConfig implements ConfigData {
 
+    @ConfigEntry.Category("feature")
     private boolean enableBridgingAssist = true;
+    @ConfigEntry.Category("feature")
+    private boolean onlyBridgeWhenCrouched = false;
+    @ConfigEntry.Category("feature")
     private boolean enableSlabAssist = true;
+    @ConfigEntry.Category("feature")
+    private boolean enableNonSolidReplace = true;
 
+
+    @ConfigEntry.Category("vfx")
     private boolean showCrosshair = true;
+    @ConfigEntry.Category("vfx")
+    private boolean showOutline = false;
+    @ConfigEntry.Category("vfx")
+    @ConfigEntry.ColorPicker(allowAlpha = true)
+    private int outlineColour = 0x66000000;  // aarrggbb
 
+
+    @ConfigEntry.Category("debug")
     private boolean showDebugHighlight = true;
+    @ConfigEntry.Category("debug")
     private boolean showDebugTrace = false;
 
 
@@ -19,11 +36,33 @@ public class BridgingConfig implements ConfigData {
         return this.enableBridgingAssist;
     }
 
+    public boolean shouldOnlyBridgeWhenCrouched() {
+        return this.onlyBridgeWhenCrouched;
+    }
+
     public boolean isSlabAssistEnabled() {
         return this.enableSlabAssist;
     }
 
-    public boolean shouldShowDebugHighlight() {
+    public boolean isNonSolidReplaceEnabled() {
+        return this.enableNonSolidReplace;
+    }
+
+
+    public boolean shouldShowCrosshair() {
+        return this.showCrosshair;
+    }
+
+    public boolean shouldShowOutline() {
+        return this.showOutline;
+    }
+
+    public int getOutlineColour() {
+        return this.outlineColour;
+    }
+
+
+    public boolean shouldShowOutlineInF3() {
         return this.showDebugHighlight;
     }
 
@@ -31,11 +70,10 @@ public class BridgingConfig implements ConfigData {
         return this.showDebugTrace;
     }
 
-    public boolean shouldShowCrosshair() {
-        return this.showCrosshair;
-    }
+
 
     public void toggleBridgingEnabled() {
         this.enableBridgingAssist = !this.isBridgingEnabled();
     }
+
 }
