abf43445b54d71580c548f607393d907dbbbd8c7
Will Scully <16578982+squeeglii@users.noreply.github.com>
2024-06-19 01:58:24 +0100

    Improve vertical slab assist + refactor & slightly improve horizontal slab assist
    Signed-off-by: Will Scully <16578982+squeeglii@users.noreply.github.com>

--- common/src/main/java/me/cg360/mod/bridging/util/GameSupport.java
+++ common/src/main/java/me/cg360/mod/bridging/util/GameSupport.java
@@ -7,6 +7,8 @@ import net.minecraft.world.item.ItemStack;
 
 public class GameSupport {
 
+    public static final double TRAPDOOR_HEIGHT = 3/16d;
+    public static final double SLAB_HEIGHT = 8/16d;
     public static final double MAXIMUM_PLACE_REACH = 4.5d;
 
     public static double getReach() {
