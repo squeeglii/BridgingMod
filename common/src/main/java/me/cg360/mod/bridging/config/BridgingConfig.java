18346be80a9410e2011351d889fe6605a7802eab
Will Scully <16578982+squeeglii@users.noreply.github.com>
2024-10-09 15:15:00 +0100

    Add extra images for promo/config use

--- common/src/main/java/me/cg360/mod/bridging/config/BridgingConfig.java
+++ common/src/main/java/me/cg360/mod/bridging/config/BridgingConfig.java
@@ -71,6 +71,7 @@ public class BridgingConfig extends DefaultValueTracker {
     private boolean skipTorchBridging = true;
     @Category("fixes") @SerialEntry
     @IncludeExtraDescription(extraParagraphs = 3)
+    @IncludeImage("textures/gui/config/bridging_slabs_horizontal.png")
     private boolean enableSlabAssist = true;
     @Category("fixes") @SerialEntry
     private boolean enableNonSolidReplace = true;
