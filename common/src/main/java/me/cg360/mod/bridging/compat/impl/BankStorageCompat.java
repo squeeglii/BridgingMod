01ea01efa0da93a3bcde02a36f3515e377950947
Will Scully <16578982+squeeglii@users.noreply.github.com>
2025-01-22 16:39:24 +0000

    Fix BankStorage compat - oops

--- common/src/main/java/me/cg360/mod/bridging/compat/impl/BankStorageCompat.java
+++ common/src/main/java/me/cg360/mod/bridging/compat/impl/BankStorageCompat.java
@@ -1,20 +1,21 @@
 package me.cg360.mod.bridging.compat.impl;
 
 import me.cg360.mod.bridging.compat.SpecialHandlers;
+import me.cg360.mod.bridging.compat.handler.BankStorageHandler;
 import me.cg360.mod.bridging.compat.handler.PlaceableItemHandler;
 import net.minecraft.resources.ResourceLocation;
 
 public class BankStorageCompat {
 
     public BankStorageCompat() {
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_1"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_2"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_3"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_4"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_5"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_6"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_7"), PlaceableItemHandler.INSTANCE);
-        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_link"), PlaceableItemHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_1"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_2"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_3"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_4"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_5"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_6"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_7"), BankStorageHandler.INSTANCE);
+        SpecialHandlers.registerSpecialHandler(ResourceLocation.fromNamespaceAndPath("bankstorage", "bank_link"), BankStorageHandler.INSTANCE);
     }
 
 }
