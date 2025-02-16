0bb65126c8304ed40a375023dbdfafea739e3186
Will <16578982+CloudG360@users.noreply.github.com>
2025-02-16 16:30:20 +0000

    Fix crash on servers in BankStorage compat

--- common/src/main/java/me/cg360/mod/bridging/compat/handler/BankStorageHandler.java
+++ common/src/main/java/me/cg360/mod/bridging/compat/handler/BankStorageHandler.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.natte.bankstorage.BankStorage;
 import net.natte.bankstorage.container.BankItemStorage;
+import net.natte.bankstorage.container.CachedBankStorage;
 import net.natte.bankstorage.options.BankOptions;
 import net.natte.bankstorage.util.Util;
 
@@ -32,7 +33,6 @@ public class BankStorageHandler implements SpecialBridgingHandler {
 
         // TODO: Look for ItemComponents instead of an item type.
         if(!(Util.isBankLike(stack))) {
-            // not a dank bank. This compatibility class does nothing!
             ResourceLocation id = BuiltInRegistries.ITEM.getKey(item);
             LogUtils.getLogger().warn("Blocked using BankStorage compatibility for an unsupported item (%s)!".formatted(id));
             return false;
@@ -43,7 +43,7 @@ public class BankStorageHandler implements SpecialBridgingHandler {
             return false;
         }
 
-        BankItemStorage storage = Util.getBankItemStorage(stack);
+        CachedBankStorage storage = CachedBankStorage.getBankStorage(stack);
         if(storage == null) {
             return false;
         }
@@ -59,7 +59,7 @@ public class BankStorageHandler implements SpecialBridgingHandler {
 
     @Override
     public boolean canBePlacedInWorld(ItemStack stack, Player player, Level level, BlockPos pos, Direction direction) {
-        BankItemStorage storage = Util.getBankItemStorage(stack);
+        CachedBankStorage storage = CachedBankStorage.getBankStorage(stack);
         if(storage == null) {
             return false;
         }
@@ -74,7 +74,7 @@ public class BankStorageHandler implements SpecialBridgingHandler {
 
     @Override
     public BlockHitResult generatePlacementTarget(ItemStack stack, Player player, Level level, Direction direction, BlockPos pos) {
-        BankItemStorage storage = Util.getBankItemStorage(stack);
+        CachedBankStorage storage = CachedBankStorage.getBankStorage(stack);
         if(storage == null) {
             return null;
         }
