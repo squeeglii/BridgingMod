8c851cf59542258de6af17c5c25bec857902f89f
Cassian Godsted <cassian@godsted.com>
2025-10-12 20:19:05 -0400

    Port to 1.21.9/10

--- common/src/main/java/me/cg360/mod/bridging/BridgingKeyMappings.java
+++ common/src/main/java/me/cg360/mod/bridging/BridgingKeyMappings.java
@@ -2,17 +2,20 @@ package me.cg360.mod.bridging;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
+import static me.cg360.mod.bridging.BridgingMod.MOD_ID;
+
 public class BridgingKeyMappings {
 
     private static final ArrayList<KeyMapping> KEY_MAPPINGS = new ArrayList<>();
+    private static final KeyMapping.Category CATEGORY = new KeyMapping.Category(ResourceLocation.fromNamespaceAndPath(MOD_ID, "category"));
 
-
-    public static final KeyMapping TOGGLE_BRIDGING = defineMapping(new KeyMapping("key.bridgingmod.toggle_bridging", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_COMMA, "key.categories.bridgingmod"));
+    public static final KeyMapping TOGGLE_BRIDGING = defineMapping(new KeyMapping("key.bridgingmod.toggle_bridging", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_COMMA, CATEGORY));
 
 
 
